5f9b8a0bd4028a997e92c5e5c72f4facb8f40f82
ArgyrisPap <arg.papa97@gmail.com>
2026-01-08 19:34:26 +0200

    testing ci/cd v.2.1

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ echo "Starting Deployment..."
 cd ~/DS-LAB
 git pull origin main
 #Θελω να ειναι ελαφρυς ο σερβερ-γιατι το build ειναι βαρυ (θα ειναι κατω καθολη τη διαρκεια ή εαν αποτυχει)
-sudo docker-compose down
+docker-compose down
 
 ./mvnw clean package -DskipTests
 #μεσα στο docker γινεται το copy του νεου .jar
-sudo docker-compose up --build -d
+docker-compose up --build -d
 #προαιρετικο για να βλεπω τα logs του app
 #sudo docker-compose logs -f app #χωρις το app, για να δω logs nginx,app,db
-sudo docker image prune -f
+docker image prune -f
 
 echo "Deployment Finished! Site is up"
