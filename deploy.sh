018b311b08d41580ee298566b9256d3230ed5403
ArgyrisPap <arg.papa97@gmail.com>
2026-01-08 19:19:02 +0200

    testing ci/cd v.2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Starting Deployment..."
+cd ~/DS-LAB
+git pull origin main
 #Θελω να ειναι ελαφρυς ο σερβερ-γιατι το build ειναι βαρυ (θα ειναι κατω καθολη τη διαρκεια ή εαν αποτυχει)
 sudo docker-compose down
 
@@ -8,4 +11,6 @@ sudo docker-compose down
 sudo docker-compose up --build -d
 #προαιρετικο για να βλεπω τα logs του app
 #sudo docker-compose logs -f app #χωρις το app, για να δω logs nginx,app,db
-sudo docker-compose logs -f
+sudo docker image prune -f
+
+echo "Deployment Finished! Site is up"
